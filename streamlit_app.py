d6fa16fa4fa59c0024d476fbf10dc555165d0b79
shivam bali <68219593+Shivambali841@users.noreply.github.com>
2022-08-02 15:28:43 +0530

    Update streamlit_app.py
    build smoothie added

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.header('breakfast menu')
 streamlit.text('omega 3, blueberry oatmeal')
 streamlit.text('kale, spinach')
 streamlit.text('hard-boiled egg')
+streamlit.text('avacado toast')
+
+
+streamlit.title('Build your own smoothie')
