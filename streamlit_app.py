dabe868ad6176460524e53cb7bad0ff131515ad1
shivam bali <68219593+Shivambali841@users.noreply.github.com>
2022-08-02 15:25:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("new diner")
+streamlit.header('breakfast menu')
+streamlit.text('omega 3, blueberry oatmeal')
+streamlit.text('kale, spinach')
+streamlit.text('hard-boiled egg')
