923aacb765c1f7025bf110cb218a059a8b01559d
shivam bali <68219593+Shivambali841@users.noreply.github.com>
2022-08-02 15:33:39 +0530

    Update streamlit_app.py
    added the dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,5 @@ streamlit.text('avacado toast')
 
 streamlit.title('Build your own smoothie')
 import pandas as pd
+my_fruit_list=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
